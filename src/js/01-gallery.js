ab4667c7eb90376560b869496e5f1729ec41bcd9
DenShykyta <wrkacc4@gmail.com>
2023-03-07 14:44:55 +0200

    ДЗ 8 v.1.0

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,51 @@
 // Add imports above this line
+import SimpleLightbox from "simplelightbox";
+
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 import { galleryItems } from './gallery-items';
+
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+const galleryMarkup = createGalleryMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+
+function createGalleryMarkup(galleryItems) {
+    return galleryItems.map(({original,preview,description})=> {
+        return `
+    <div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>
+`
+    }).join('');
+}
+
+
+galleryContainer.addEventListener('click', onImageClick);
+
+function onImageClick(e) {
+  e.preventDefault();
+  if (e.target.nodeName !== "IMG") {
+    return;
+  };
+
+  basicLightbox.create(`
+		<img width="1400" height="900" src="${e.target.dataset.source}">
+	`).show();
+
+};
+
+const lightbox = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionPosition: 'bottom', captionDelay: 250 });
+// console.log(galleryItems);
+
+
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,42 @@
+import throttle from 'lodash.throttle'
 
+const refs = {
+    form: document.querySelector('.feedback-form'),
+    email: document.querySelector('[name = "email"]'),
+    message: document.querySelector('[name = "message"]'),
+};
+
+
+
+const STORAGE_DATA = 'feedback-data';
+const dataSave = {};
+
+populateInput();
+
+refs.form.addEventListener('input', throttle(onFormInput, 500));
+refs.form.addEventListener('submit', onFormSubmit);
+
+function onFormInput(evt) {
+    
+    dataSave[evt.target.name] = evt.target.value;
+
+    localStorage.setItem(STORAGE_DATA, JSON.stringify(dataSave));
+}
+
+function onFormSubmit(evt) {
+    evt.preventDefault();
+
+    evt.currentTarget.reset();
+
+    localStorage.removeItem(STORAGE_DATA);
+
+}
+
+function populateInput() {
+    const savedLocalData = JSON.parse(localStorage.getItem(STORAGE_DATA));
+    
+    if (savedLocalData) {
+        refs.email.value = savedLocalData.email;
+        refs.message.value = savedLocalData.message;
+    }
+}
